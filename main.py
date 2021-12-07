b847629be88648d6432f8577ca7f5629510f727d
irina <itcvetkova@gmail.com>
2021-12-07 19:27:44 +0300

    test1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def my_superfunction():
 
 def main():
     print('My first git program')
-    print('And I change it every day')
+    print('every day')
     print('Again')
     print('UFO came and added this line')
     my_superfunction()
