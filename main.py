d3c3f06e1b2eb510a22b522fdcec55fe5411fec0
irina <itcvetkova@gmail.com>
2021-12-07 19:54:29 +0300

    test5

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Yandexlyceum
+
 def my_superfunction():
     print('What an awesome print!')
 
